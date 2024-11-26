bef6fb1623b611fd53fc9c2a4d32662492066ea9
bisht-xp <bishtkamal2907@gmail.com>
2024-11-26 18:00:07 +0530

    updated: updated src file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { closestCorners, DndContext } from "@dnd-kit/core";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useDispatch, useSelector } from "react-redux";
+import { updateProductOrder } from "./store/features/product/productSlice";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="w-full h-[100vh] flex flex-col">
+      <Navbar />
+      <div className="w-full mt-16">
+        <DndContext collisionDetection={closestCorners}>
+          <ProductList />
+        </DndContext>
+      </div>
+    </div>
+  );
 }
